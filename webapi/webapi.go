f599cb85d96284e7967fa7e5751f2244f89fa1fd
Arne Ziegler <arne.ziegler@gmx.net>
2025-04-04 21:51:15 +0200

    [UPDATE] second block of add system - bug

--- webapi/webapi.go
+++ webapi/webapi.go
@@ -14,10 +14,20 @@ type AuthRequest struct {
 	Password string `json:"password"`
 }
 
+type ResultSystemGetId struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type ResponseSystemGetId struct {
+	Success bool                `json:"success"`
+	Result  []ResultSystemGetId `json:"result"`
+}
+
 type AddRemoveSystem struct {
-	systemGroupName string `json:"systemGroupName"`
-	serverIDs       string `json:"serverIDs"`
-	add             string `json:"add"`
+	SystemGroupName string `json:"systemGroupName"`
+	ServerIDs       string `json:"serverIDs"`
+	Add             string `json:"add"`
 }
 
 func Login(username, password, susemgr string) string {
@@ -86,10 +96,14 @@ func Login(username, password, susemgr string) string {
 func AddSystem(sessioncookie, susemgr, hostname, group string) (string, error) {
 
 	fmt.Printf("Session Cookie in Addsystem %s\n", sessioncookie)
+
 	// Define the API endpoint
 	apiURL := fmt.Sprintf("%s%s", susemgr, "/rhn/manager/api")
 	fmt.Println("apiURL:", apiURL)
 
+	/*
+	 check if system is registered
+	*/
 	apiMethodGetSystemId := fmt.Sprintf("%s%s%s", apiURL, "/system/getId?name=", hostname)
 	fmt.Println("apiMethod:", apiMethodGetSystemId)
 
@@ -130,7 +144,76 @@ func AddSystem(sessioncookie, susemgr, hostname, group string) (string, error) {
 	}
 
 	// Print the response
-	fmt.Printf("Response:\n%s\n", string(bodyBytes))
+	//fmt.Printf("Response:\n%s\n", string(bodyBytes))
+
+	// Unmarshal the JSON response into the struct
+	var rsp ResponseSystemGetId
+	err = json.Unmarshal(bodyBytes, &rsp)
+	if err != nil {
+		fmt.Printf("Error unmarshaling JSON: %s\n", err)
+		os.Exit(1)
+	}
+
+	// Extract and print all fields
+	//var foundHostname string
+	var foundId int
+	for _, r := range rsp.Result {
+		//foundHostname = r.Name
+		foundId = r.Id
+	}
+
+	if foundId == 0 {
+		fmt.Fprintf(os.Stderr, "Host: %s not found in SUSE Manager on %s\n", hostname, susemgr)
+		os.Exit(1)
+	}
+
+	/*
+	 add System to Group
+	*/
+	apiMethodAddOrRemoveSystems := fmt.Sprintf("%s%s", apiURL, "/systemgroup/addOrRemoveSystems")
+	fmt.Println("apiMethod:", apiMethodAddOrRemoveSystems)
+
+	// Create the authentication request payload
+	fId := fmt.Sprintf("%d", foundId)
+	AddRemoveSystemPayload := AddRemoveSystem{
+		SystemGroupName: group,
+		ServerIDs:       fId,
+		Add:             "True",
+	}
+
+	// Marshal the payload to JSON
+	payloadBytes, err := json.Marshal(AddRemoveSystemPayload)
+	if err != nil {
+		fmt.Printf("Error marshalling payload: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("\nPaylod: %v\n", string(payloadBytes))
+
+	// Create an HTTP POST request
+	req, err = http.NewRequest("POST", apiMethodAddOrRemoveSystems, bytes.NewBuffer(payloadBytes))
+	if err != nil {
+		fmt.Printf("Error creating request: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Add headers
+	req.Header.Set("Content-Type", "application/json")
+	req.AddCookie(&http.Cookie{
+		Name:  "pxt-session-cookie",
+		Value: sessioncookie,
+	})
+
+	// Send the request using the HTTP client
+	client = &http.Client{}
+	resp, err = client.Do(req)
+	if err != nil {
+		fmt.Printf("Error sending request: %v\n", err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	fmt.Printf("\n\nAdd Node: %v\n", resp)
 
 	return string(bodyBytes), nil
 
